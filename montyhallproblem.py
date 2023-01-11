44060ce35cd30fd03424cb7afb2c1345b64432ee
yjh0816 <2543kt@naver.com>
2023-01-11 10:54:10 +0900

    feat: Count the stay, change case
    The problem solving is too easy. but not for me

--- montyhallproblem.py
+++ montyhallproblem.py
@@ -13,6 +13,14 @@ doors = [
 # Loop 200 cycle for calculating win rate each stay, change case
 for i in range(1,200+1):
   shuffle(doors)
-  print(doors) 
+  # print(doors) 
   user_choice = choice(doors)
-  print(user_choice)
+  # print(user_choice)
+
+  # Except user's choice, check the goat door
+  if user_choice == 0:
+      c_count += 1
+  else:
+      s_count += 1
+
+print("s_count:",s_count,"c_count:",c_count)
