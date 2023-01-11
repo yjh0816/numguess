115aa530f2a3dfeae669e30b516a0fbd64748581
yjh0816 <2543kt@naver.com>
2023-01-11 16:34:40 +0900

    feat: Give 5 chance for answering
    I make loop and functionize answering sequence

--- numguess.py
+++ numguess.py
@@ -1,36 +1,41 @@
 from random import randint
 from time import sleep
 
-# Generate random answer
-answer = randint(1,100)
-
-# Print answer
-print("The answer is", answer)
-
 # Get user name from user
 username = input("Input your name: ")
 
 # Say hello to user, using username
 print(f"Nice to meet you,{username}")
 
+# Generate random answer
+answer = randint(1,100)
+
+# Print answer
+print("The answer is", answer)
+
+def compare_answer(guess):
+    # Print user's guess
+    print(f"{username}'s guess is {guess}")
+
+    # Compare answer with user's guess
+    if guess == answer:
+        print("******************")
+        sleep(1)
+        print("******************")
+        sleep(1)
+        print("******************")
+        sleep(1)
+        print(f"Collect! The answer is {answer}!!")
+        return True        
+    elif guess > answer:
+        print("The answer is lower than your guess")
+    elif guess < answer:
+        print("The answer is higher than your guess")
+    else:
+        print(f"Wrong answer. The answer is {answer}, {username}..")
+
 # Get user guess from user
-guess = int(input(f"So {username}, Guess the number(between 1~100): "))
-
-# Print user's guess
-print(f"{username}'s guess is {guess}")
-
-# Compare answer with user's guess
-if guess == answer:
-    print("******************")
-    sleep(1)
-    print("******************")
-    sleep(1)
-    print("******************")
-    sleep(1)
-    print(f"Collect! The answer is {answer}!!")
-elif guess > answer:
-    print("The answer is lower than your guess")
-elif guess < answer:
-    print("The answer is higher than your guess")
-else:
-    print(f"Wrong answer. The answer is {answer}, {username}..")
+for i in range(5):
+    num = int(input(f"So {username}, Guess the number(between 1~100): "))
+    if compare_answer(num):
+        break
